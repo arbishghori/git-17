0b6e09b31d3a354f62bf2499d9182eae0dac09c1
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-21 18:14:32 +0500

    Add files via upload

---
+++ git 17/script.js
@@ -0,0 +1,10 @@
+//Practice placement of <script></script> element in
+//following sections of your project in exercise 6:
+//a. Head
+//b. Body (before your page’s HTML)
+//c. Body (inside your page’s HTML)
+//d. Body (after your page’s HTML)
+// answer 7
+
+
+//=============================on html page =====================================
